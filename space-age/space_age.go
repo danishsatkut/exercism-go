4f97f3321a0805391e63f10ceb2bdb66ecdeae54
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-08-11 22:49:45 +0530

    Refactor to remove switch case.

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -6,25 +6,21 @@ type Planet string
 
 const earthYearInSeconds = 60 * 60 * 24 * 365.25
 
+var planetEarthYears = map[Planet]float64{
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Earth":   1,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
+
 func Age(s float64, p Planet) float64 {
-	switch p {
-	case "Earth":
-		return s / earthYearInSeconds
-	case "Mercury":
-		return s / earthYearInSeconds / 0.2408467
-	case "Venus":
-		return s / earthYearInSeconds / 0.61519726
-	case "Mars":
-		return s / earthYearInSeconds / 1.8808158
-	case "Jupiter":
-		return s / earthYearInSeconds / 11.862615
-	case "Saturn":
-		return s / earthYearInSeconds / 29.447498
-	case "Uranus":
-		return s / earthYearInSeconds / 84.016846
-	case "Neptune":
-		return s / earthYearInSeconds / 164.79132
-	default:
-		panic(fmt.Sprint("Invalid planet: ", p))
+	if earthYears, ok := planetEarthYears[p]; ok {
+		return s / (earthYearInSeconds * earthYears)
 	}
+
+	panic(fmt.Sprint("Invalid planet: ", p))
 }
