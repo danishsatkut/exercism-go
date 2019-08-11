8087dc6e1ccb1a8a3f5904d80fd75705cfbdc71b
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-08-11 22:58:09 +0530

    Calculate planet years in advance.

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -7,19 +7,19 @@ type Planet string
 const earthYearInSeconds = 60 * 60 * 24 * 365.25
 
 var planetEarthYears = map[Planet]float64{
-	"Mercury": 0.2408467,
-	"Venus":   0.61519726,
-	"Earth":   1,
-	"Mars":    1.8808158,
-	"Jupiter": 11.862615,
-	"Saturn":  29.447498,
-	"Uranus":  84.016846,
-	"Neptune": 164.79132,
+	"Mercury": 0.2408467 * earthYearInSeconds,
+	"Venus":   0.61519726 * earthYearInSeconds,
+	"Earth":   1 * earthYearInSeconds,
+	"Mars":    1.8808158 * earthYearInSeconds,
+	"Jupiter": 11.862615 * earthYearInSeconds,
+	"Saturn":  29.447498 * earthYearInSeconds,
+	"Uranus":  84.016846 * earthYearInSeconds,
+	"Neptune": 164.79132 * earthYearInSeconds,
 }
 
 func Age(s float64, p Planet) float64 {
-	if earthYears, ok := planetEarthYears[p]; ok {
-		return s / (earthYearInSeconds * earthYears)
+	if yearsInSecond, ok := planetEarthYears[p]; ok {
+		return s / yearsInSecond
 	}
 
 	panic(fmt.Sprint("Invalid planet: ", p))
