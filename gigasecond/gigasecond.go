d927a9a2f8c8ef22e08d0a9e724fe0ddb6eac50f
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-08-12 13:25:49 +0530

    Implement AddGigasecond.

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -6,13 +6,14 @@
 package gigasecond
 
 // import path for the time package from the standard library
-import "time"
+import (
+	"math"
+	"time"
+)
 
 // AddGigasecond should have a comment documenting it.
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t
+	gigasecond := time.Duration(math.Pow10(9)) * time.Second
+
+	return t.Add(gigasecond)
 }
