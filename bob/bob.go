3ab1dab71c88682bfd0ca715ba9c45522c54b2d1
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-08-12 21:03:51 +0530

    Implement Hey function for bob.

--- bob/bob.go
+++ bob/bob.go
@@ -5,11 +5,36 @@
 // https://golang.org/doc/effective_go.html#commentary
 package bob
 
+import "strings"
+
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	remark = strings.TrimSpace(remark)
+
+	if remark == "" {
+		return "Fine. Be that way!"
+	}
+
+	if strings.HasSuffix(remark, "?") {
+		if strings.ToUpper(remark) == strings.ToLower(remark) {
+			return "Sure."
+		}
+
+		if strings.ToUpper(remark) == remark {
+			return "Calm down, I know what I'm doing!"
+		}
+
+		return "Sure."
+	}
+
+
+	if strings.ToUpper(remark) == remark {
+		if strings.ToUpper(remark) == strings.ToLower(remark) {
+			return "Whatever."
+		}
+
+		return "Whoa, chill out!"
+	}
+
+	return "Whatever."
 }
