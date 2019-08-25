43b8db88f6421bfebb9bb132be8f54f3e94b7c80
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-08-25 19:33:02 +0530

    Refactor to use FieldsFunc.

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -4,23 +4,18 @@ package acronym
 
 import (
 	"strings"
+	"unicode"
 )
 
 // Abbreviate returns abbreviation for a long name.
 func Abbreviate(s string) (abbr string) {
-	for _, word := range strings.Fields(s) {
-		if strings.Contains(word, "-") {
-			for _, w := range strings.Split(word, "-") {
-				abbr += Abbreviate(w)
-			}
-		} else if strings.Contains(word, "_") {
-			for _, w := range strings.Split(word, "_") {
-				abbr += Abbreviate(w)
-			}
-		} else {
-			abbr += string(word[0])
-		}
+	for _, word := range strings.FieldsFunc(s, isSeparator) {
+		abbr += string(word[0])
 	}
 
 	return strings.ToUpper(abbr)
+}
+
+func isSeparator(r rune) bool {
+	return unicode.IsSpace(r) || string(r) == "-" || string(r) == "_"
 }
