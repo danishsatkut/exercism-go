46ad1540270762bc80991733ab50d0bcb9842ce5
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-08-25 21:07:24 +0530

    Add comments.

--- leap/leap.go
+++ leap/leap.go
@@ -1,11 +1,7 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package leap provides functionality to calculate leap years
 package leap
 
-// IsLeapYear should have a comment documenting it.
+// IsLeapYear returns true if the year is leap or false otherwise.
 func IsLeapYear(year int) bool {
 	return divisibleBy(year, 4) &&
 		(!divisibleBy(year, 100) || divisibleBy(year, 400))
