3fbfef2d43c4e6f797a320a203816a28c7d2e4dd
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-08-25 23:11:04 +0530

    Implement functionality to figure out kind of triangle.

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -5,24 +5,50 @@
 // https://golang.org/doc/effective_go.html#commentary
 package triangle
 
+import "math"
 
 // Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind int
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	// Pick values for the following identifiers used by the test program.
+	NaT Kind = iota // not a triangle
+	Equ             // equilateral
+	Iso             // isosceles
+	Sca             // scalene
 )
 
 // KindFromSides should have a comment documenting it.
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
 	var k Kind
+
+	if a <= 0 || b <= 0 || c <= 0 {
+		return NaT
+	}
+
+	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
+		return NaT
+	}
+
+	if math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) {
+		return NaT
+	}
+
+	if (a+b < c) || (a+c < b) || (b+c < a) {
+		return NaT
+	}
+
+	if a == b && b == c {
+		return Equ
+	}
+
+	if (a != b && b == c) || (a != b && a == c) || (a != c && a == b) {
+		return Iso
+	}
+
+	if a != b && a != c && b != c {
+		return Sca
+	}
+
 	return k
 }
