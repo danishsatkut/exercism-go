babe73cce0ac87a26f8924fca6a6150eeac0b4c0
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-08-25 20:33:11 +0530

    Implement hamming distance.

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,19 @@
 package hamming
 
-func Distance(a, b string) (int, error) {
+import "fmt"
+
+func Distance(strand, otherStrand string) (int, error) {
+	var distance = 0
+
+	if len(strand) != len(otherStrand) {
+		return 0, fmt.Errorf("strands contain different number of nucleotides")
+	}
+
+	for i := 0; i < len(strand); i++ {
+		if strand[i] != otherStrand[i] {
+			distance += 1
+		}
+	}
+
+	return distance, nil
 }
