48317f600259d55f01fcb5420fba48b6d222d978
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-08-26 16:09:14 +0530

    Refactor to extract isSideInvalid function.

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -22,15 +22,7 @@ const (
 func KindFromSides(a, b, c float64) Kind {
 	var k Kind
 
-	if a <= 0 || b <= 0 || c <= 0 {
-		return NaT
-	}
-
-	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
-		return NaT
-	}
-
-	if math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) {
+	if isSideInvalid(a) || isSideInvalid(b) || isSideInvalid(c) {
 		return NaT
 	}
 
@@ -51,4 +43,8 @@ func KindFromSides(a, b, c float64) Kind {
 	}
 
 	return k
+}
+
+func isSideInvalid(s float64) bool {
+	return s <= 0 || math.IsNaN(s) || math.IsInf(s, 0)
 }
