86eff628b90d6b5ac3183dc67ea217a8feebe33a
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-09-08 14:43:55 +0530

    Implement proverb functionality.

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -5,11 +5,23 @@
 // https://golang.org/doc/effective_go.html#commentary
 package proverb
 
+import "fmt"
+
 // Proverb should have a comment documenting it.
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+	var lines []string
+
+	if len(rhyme) == 0 {
+		return lines
+	}
+
+	for i := 0; i < len(rhyme) - 1; i++ {
+		line := fmt.Sprintf("For want of a %v the %v was lost.", rhyme[i], rhyme[i+1])
+
+		lines = append(lines, line)
+	}
+
+	lines = append(lines, fmt.Sprintf("And all for the want of a %v.", rhyme[0]))
+
+	return lines
 }
