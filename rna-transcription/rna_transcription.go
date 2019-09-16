9c5ac8aeb584f0f0ca13e5f2e0d8c2e68bb4a06e
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-09-16 20:56:00 +0530

    Refactor to use map instead of switch.

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,35 +1,25 @@
 // Package strand provides functionality related to DNA and RNA strands.
 package strand
 
-import "errors"
+var rnaTranscriptions = map[string]string{
+	"C": "G",
+	"G": "C",
+	"T": "A",
+	"A": "U",
+}
 
 // ToRNA converts a DNA strand into a RNA strand
 func ToRNA(dna string) string {
 	var rna string
 
 	for _, nucleotide := range dna {
-		n, err := toRNANucleotide(string(nucleotide))
-		if err != nil {
-			panic(err)
+		n, ok := rnaTranscriptions[string(nucleotide)]
+		if !ok {
+			panic("invalid DNA dnaNucleotide: " + string(nucleotide))
 		}
 
 		rna += n
 	}
 
 	return rna
-}
-
-func toRNANucleotide(dnaNucleotide string) (string, error) {
-	switch dnaNucleotide {
-	case "C":
-		return "G", nil
-	case "G":
-		return "C", nil
-	case "T":
-		return "A", nil
-	case "A":
-		return "U", nil
-	default:
-		return "", errors.New("invalid DNA nucleotide: " + dnaNucleotide)
-	}
 }
