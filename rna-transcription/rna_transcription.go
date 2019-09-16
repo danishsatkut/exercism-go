d32a0bcacc18f36ac66f0202371df5bb3208273e
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-09-16 21:09:30 +0530

    Refactor to use string replacer.

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,25 +1,16 @@
 // Package strand provides functionality related to DNA and RNA strands.
 package strand
 
-var rnaTranscriptions = map[string]string{
-	"C": "G",
-	"G": "C",
-	"T": "A",
-	"A": "U",
-}
+import "strings"
+
+var rnaTranscriptions = strings.NewReplacer(
+	"G", "C",
+	"C", "G",
+	"T", "A",
+	"A", "U",
+)
 
 // ToRNA converts a DNA strand into a RNA strand
 func ToRNA(dna string) string {
-	var rna string
-
-	for _, nucleotide := range dna {
-		n, ok := rnaTranscriptions[string(nucleotide)]
-		if !ok {
-			panic("invalid DNA dnaNucleotide: " + string(nucleotide))
-		}
-
-		rna += n
-	}
-
-	return rna
+	return rnaTranscriptions.Replace(dna)
 }
