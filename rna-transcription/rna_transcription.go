3315fe1319dee68bfa37af4daa9eb2d846e20c16
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-09-16 18:22:20 +0530

    Implement RNA transcription.

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,4 +1,33 @@
 package strand
 
+import "errors"
+
 func ToRNA(dna string) string {
+	var rna string
+
+	for _, nucleotide := range dna {
+		n, err := toRNANucleotide(string(nucleotide))
+		if err != nil {
+			panic(err)
+		}
+
+		rna += n
+	}
+
+	return rna
+}
+
+func toRNANucleotide(dnaNucleotide string) (string, error) {
+	switch dnaNucleotide {
+	case "C":
+		return "G", nil
+	case "G":
+		return "C", nil
+	case "T":
+		return "A", nil
+	case "A":
+		return "U", nil
+	default:
+		return "", errors.New("invalid DNA nucleotide: " + dnaNucleotide)
+	}
 }
