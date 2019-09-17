506b5e881d79a6ffc1c8be2258531a4923e782f2
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-09-17 21:45:56 +0530

    Implement scrable score.

---
+++ scrabble-score/scrabble_score.go
@@ -0,0 +1,34 @@
+package scrabble
+
+import "strings"
+
+var alphabetScores = map[int]string{
+	1:  "AEIOULNRST",
+	2:  "DG",
+	3:  "BCMP",
+	4:  "FHVWY",
+	5:  "K",
+	8:  "JX",
+	10: "QZ",
+}
+
+func Score(word string) int {
+	var score int
+	letters := []rune(strings.ToUpper(word))
+
+	for _, l := range letters {
+		score += letterScore(l)
+	}
+
+	return score
+}
+
+func letterScore(letter rune) int {
+	for score, alphabets := range alphabetScores {
+		if strings.ContainsRune(alphabets, letter) {
+			return score
+		}
+	}
+
+	panic("not a valid alphabet: " + string(letter))
+}
