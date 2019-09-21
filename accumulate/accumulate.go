2f2529649e3a5740a28def719b6aa047ba86d79c
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-09-21 23:15:35 +0530

    Refactor to introduce Mapper type.

--- accumulate/accumulate.go
+++ accumulate/accumulate.go
@@ -1,13 +1,16 @@
 // Package accumulate provides functionality to accumulate results of operations
 package accumulate
 
+// Mapper is function that maps a string to another string
+type Mapper func(string) string
+
 // Accumulate performs an operation on items of list and returns
 // a list of results.
-func Accumulate(list []string, op func(string) string) []string {
+func Accumulate(list []string, mapper Mapper) []string {
 	result := make([]string, 0, len(list))
 
 	for _, s := range list {
-		result = append(result, op(s))
+		result = append(result, mapper(s))
 	}
 
 	return result
