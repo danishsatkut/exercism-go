35f070a2be6df7da9fcb20e9f093fbb832b86121
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-09-21 23:07:13 +0530

    Add comments.

--- accumulate/accumulate.go
+++ accumulate/accumulate.go
@@ -1,5 +1,8 @@
+// Package accumulate provides functionality to accumulate results of operations
 package accumulate
 
+// Accumulate performs an operation on items of list and returns
+// a list of results.
 func Accumulate(list []string, op func(string) string) []string {
 	result := make([]string, 0, len(list))
 
