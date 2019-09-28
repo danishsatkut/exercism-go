0ad057a67015a8c36f3e613a719480ccbb9a08fc
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-09-28 22:25:09 +0530

    Add Strings type with Keep and Discard methods.

--- strain/strain.go
+++ strain/strain.go
@@ -9,3 +9,13 @@ func (i Ints) Keep(func(int) bool) Ints {
 func (i Ints) Discard(func(int) bool) Ints {
 	return Ints{}
 }
+
+type Strings []string
+
+func (s Strings) Keep(func(string) bool) Strings {
+	return Strings{}
+}
+
+func (s Strings) Discard(func(string) bool) Strings {
+	return Strings{}
+}
