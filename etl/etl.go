763bea3702a4cfdcb041f2f8a854d45e7907a3bd
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-09-30 00:26:43 +0530

    Refactor to rename result variable to output.

--- etl/etl.go
+++ etl/etl.go
@@ -4,13 +4,13 @@ import "strings"
 
 // Transform performs transformation for scrabble score.
 func Transform(scores map[int][]string) map[string]int {
-	var t = make(map[string]int)
+	var output = make(map[string]int)
 
 	for score, letters := range scores {
 		for _, l := range letters {
-			t[strings.ToLower(l)] = score
+			output[strings.ToLower(l)] = score
 		}
 	}
 
-	return t
+	return output
 }
