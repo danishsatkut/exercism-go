5bef36151cbd4a26aab090957bdba6141fda2455
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-09-30 00:14:56 +0530

    Implement Transform functionality.

---
+++ etl/etl.go
@@ -0,0 +1,15 @@
+package etl
+
+import "strings"
+
+func Transform(scores map[int][]string) map[string]int {
+	var t = make(map[string]int)
+
+	for score, letters := range scores {
+		for _, l := range letters {
+			t[strings.ToLower(l)] = score
+		}
+	}
+
+	return t
+}
