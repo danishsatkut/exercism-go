3116bc0b2582f41f12339c9a46ea1bdc3648b90a
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-10-06 22:43:19 +0530

    Add comments.

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -2,6 +2,8 @@ package pangram
 
 import "unicode"
 
+// IsPangram determines if a sentence is a pangram.
+// A pangram is a sentence using every letter of the alphabet at least once.
 func IsPangram(sentence string) bool {
 	if sentence == "" {
 		return false
