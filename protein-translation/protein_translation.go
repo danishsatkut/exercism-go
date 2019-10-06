8e8bd331ec1e438c89688a87d06870da21517bd9
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-10-06 21:09:51 +0530

    Implement FromRNA function.

--- protein-translation/protein_translation.go
+++ protein-translation/protein_translation.go
@@ -1,11 +1,34 @@
 package protein
 
-import "errors"
+import (
+	"errors"
+)
 
 var (
 	ErrStop        = errors.New("terminating codon")
 	ErrInvalidBase = errors.New("invalid base")
 )
+
+func FromRNA(rna string) ([]string, error) {
+	var polypeptide []string
+
+	for i := 0; i < len(rna); i += 3 {
+		codon := rna[i:i+3]
+
+		protein, err := FromCodon(codon)
+		if err != nil {
+			if err == ErrStop {
+				return polypeptide, nil
+			}
+
+			return polypeptide, err
+		}
+
+		polypeptide = append(polypeptide, protein)
+	}
+
+	return polypeptide, nil
+}
 
 func FromCodon(codon string) (string, error) {
 	switch codon {
