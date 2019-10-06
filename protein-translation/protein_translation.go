5b239fb3e5b5bc0a3995bd40caa61eddc2c3e60f
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-10-06 20:26:14 +0530

    Implement FromCodon without terminating codons.

---
+++ protein-translation/protein_translation.go
@@ -0,0 +1,22 @@
+package protein
+
+func FromCodon(codon string) (string, error) {
+	switch codon {
+	case "AUG":
+		return "Methionine", nil
+	case "UUU", "UUC":
+		return "Phenylalanine", nil
+	case "UUA", "UUG":
+		return "Leucine", nil
+	case "UCU", "UCC", "UCA", "UCG":
+		return "Serine", nil
+	case "UAU", "UAC":
+		return "Tyrosine", nil
+	case "UGU", "UGC":
+		return "Cysteine", nil
+	case "UGG":
+		return "Tryptophan", nil
+	}
+
+	return "", nil
+}
