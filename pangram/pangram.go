6de5a6ecb0c7bebbc7ca1d655ddd33608b172edb
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-10-06 22:52:55 +0530

    Remove check for empty sentence.

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -7,10 +7,6 @@ const alphabetsInEnglish = 26
 // IsPangram determines if a sentence is a pangram.
 // A pangram is a sentence using every letter of the alphabet at least once.
 func IsPangram(sentence string) bool {
-	if sentence == "" {
-		return false
-	}
-
 	var occurence = make(map[rune]bool)
 
 	for _, l := range sentence {
