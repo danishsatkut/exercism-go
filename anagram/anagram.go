128c382b32be1b17e355459cee20497ef5b20220
Danish Aziz Satkut <danish_satkut@hotmail.com>
2019-10-13 16:46:43 +0530

    Refactor to remove isAnagram.

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -2,36 +2,33 @@ package anagram
 
 import (
 	"strings"
+	"unicode"
 )
 
 // Detect returns anagrams for a word from possible candidates.
 func Detect(word string, candidates []string) []string {
 	var anagrams = make([]string, 0, len(candidates))
 
-	for _, candidate := range candidates {
-		if isAnagram(word, candidate) {
-			anagrams = append(anagrams, candidate)
+	word = strings.ToUpper(word)
+	s := newLetterCount(word)
+
+	for i, candidate := range candidates {
+		if len(word) != len(candidate) {
+			continue
+		}
+
+		candidate = strings.ToUpper(candidate)
+
+		if word == candidate {
+			continue
+		}
+
+		if s == newLetterCount(candidate) {
+			anagrams = append(anagrams, candidates[i])
 		}
 	}
 
 	return anagrams
-}
-
-func isAnagram(word, candidate string) bool {
-	if len(word) != len(candidate) {
-		return false
-	}
-
-	word, candidate = strings.ToUpper(word), strings.ToUpper(candidate)
-
-	if word == candidate {
-		return false
-	}
-
-	s := newLetterCount(word)
-	c := newLetterCount(candidate)
-
-	return s == c
 }
 
 type letterCount [26]int
@@ -40,18 +37,10 @@ func newLetterCount(word string) letterCount {
 	var letters = letterCount{}
 
 	for _, r := range word {
-		letters[r - 'A']++
-	}
-
-	return letters
-}
-
-func (l letterCount) equal(other letterCount) bool {
-	for r, count := range l {
-		if other[r] != count {
-			return false
+		if unicode.IsLetter(r) {
+			letters[r - 'A']++
 		}
 	}
 
-	return true
+	return letters
 }
