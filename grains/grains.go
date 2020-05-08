ceb12b590da26d3c10726ab9e2ac8d9c66b95dcd
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-09 01:32:32 +0530

    Implement Square functionality.

--- grains/grains.go
+++ grains/grains.go
@@ -1,1 +1,18 @@
 package grains
+
+import (
+	"errors"
+	"math"
+)
+
+func Square(n int) (uint64, error) {
+	if n < 1 || n > 64 {
+		return 0, errors.New("invalid square")
+	}
+
+	if n == 1 {
+		return 1, nil
+	}
+
+	return uint64(math.Exp2(float64(n - 1))), nil
+}
