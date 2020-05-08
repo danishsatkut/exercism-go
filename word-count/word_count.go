fdf513a5fe87b4d1e6edeef43689d94a4bfca38b
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-08 23:21:05 +0530

    Calculate frequency using regexp.

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,16 +1,31 @@
 package wordcount
 
 import (
+	"regexp"
 	"strings"
 )
 
 type Frequency map[string]int
 
+var (
+	splitter      = regexp.MustCompile(`[\s\,\:]+`)
+	wordExtractor = regexp.MustCompile(`([\w\'])+`)
+	quotedWord = regexp.MustCompile(`^\'(\w+)\'$`)
+)
+
 func WordCount(sentence string) Frequency {
 	f := Frequency{}
 
-	for _, word := range strings.Split(sentence, " ") {
-		f[word]++
+	words := splitter.Split(sentence, -1)
+
+	for _, word := range words {
+		if w := wordExtractor.FindString(word); w != ""  {
+			if quotedWord.MatchString(w) {
+				w = quotedWord.FindStringSubmatch(w)[1]
+			}
+
+			f[strings.ToLower(w)]++
+		}
 	}
 
 	return f
