e7b9c33968cf384c0335aa5d0d33b9373c8a841f
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-09 01:39:27 +0530

    Add documentation comments for grains package.

--- grains/grains.go
+++ grains/grains.go
@@ -5,6 +5,7 @@ import (
 	"math"
 )
 
+// Square calculates the number of grains for a given square
 func Square(n int) (uint64, error) {
 	if n < 1 || n > 64 {
 		return 0, errors.New("invalid square")
@@ -17,6 +18,7 @@ func Square(n int) (uint64, error) {
 	return uint64(math.Exp2(float64(n - 1))), nil
 }
 
+// Total calculates the total number of grains for the entire chessboard
 func Total() uint64 {
 	var total uint64
 
