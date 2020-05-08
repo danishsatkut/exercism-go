1628f0c1e8f48cc48fdd1af250b493b5799874f7
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-09 03:16:36 +0530

    Implement RunLengthEncode functionality.

--- run-length-encoding/run_length_encoding.go
+++ run-length-encoding/run_length_encoding.go
@@ -1,1 +1,41 @@
 package encode
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func RunLengthEncode(input string) string {
+	var encoded string
+
+	var previous rune
+	var count = 1
+
+	for i, current := range input {
+		if current == previous {
+			count++
+		} else
+		{
+			if i != 0 {
+				if count > 1 {
+					encoded += strconv.Itoa(count)
+				}
+
+				encoded += fmt.Sprintf("%c", previous)
+				count = 1
+			}
+		}
+
+		if i == len(input) - 1 {
+			if count > 1 {
+				encoded += strconv.Itoa(count)
+			}
+
+			encoded += fmt.Sprintf("%c", current)
+		}
+
+		previous = current
+	}
+
+	return encoded
+}
