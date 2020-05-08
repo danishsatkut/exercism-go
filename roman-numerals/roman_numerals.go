6719611f16cf63784a718b93ff4df106b9f43ead
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-09 00:39:49 +0530

    Refactor to make dictionary a package variable.

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -9,27 +9,27 @@ type arabicToRoman struct {
 	roman  string
 }
 
+var dictionary = []arabicToRoman{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ToRomanNumeral(number int) (string, error) {
 	var roman string
 
 	if number <= 0 || number > 3000 {
 		return "", fmt.Errorf("number %d is not allowed", number)
-	}
-
-	dictionary := []arabicToRoman{
-		{1000, "M"},
-		{900, "CM"},
-		{500, "D"},
-		{400, "CD"},
-		{100, "C"},
-		{90, "XC"},
-		{50, "L"},
-		{40, "XL"},
-		{10, "X"},
-		{9, "IX"},
-		{5, "V"},
-		{4, "IV"},
-		{1, "I"},
 	}
 
 	for _, item := range dictionary {
