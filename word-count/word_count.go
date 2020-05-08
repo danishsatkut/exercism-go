700c01bdea9e7965f5e15af8ad736c2021a8a22b
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-08 23:21:26 +0530

    Fix formatting.

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -10,7 +10,7 @@ type Frequency map[string]int
 var (
 	splitter      = regexp.MustCompile(`[\s\,\:]+`)
 	wordExtractor = regexp.MustCompile(`([\w\'])+`)
-	quotedWord = regexp.MustCompile(`^\'(\w+)\'$`)
+	quotedWord    = regexp.MustCompile(`^\'(\w+)\'$`)
 )
 
 func WordCount(sentence string) Frequency {
@@ -19,7 +19,7 @@ func WordCount(sentence string) Frequency {
 	words := splitter.Split(sentence, -1)
 
 	for _, word := range words {
-		if w := wordExtractor.FindString(word); w != ""  {
+		if w := wordExtractor.FindString(word); w != "" {
 			if quotedWord.MatchString(w) {
 				w = quotedWord.FindStringSubmatch(w)[1]
 			}
