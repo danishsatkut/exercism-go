6ca5a096a9187ae82a39c82a1fb7d210d0f55ada
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-10 01:08:44 +0530

    Optimize by using empty struct.

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -3,12 +3,12 @@ package summultiples
 // SumMultiples calculates sum of all the unique multiples of particular
 // numbers up to but not including that number.
 func SumMultiples(upto int, numbers ...int) int {
-	uniqueMultiples := map[int]interface{}{}
+	uniqueMultiples := map[int]struct{}{}
 
 	for i := 0; i < upto; i++ {
 		for _, number := range numbers {
 			if multiple := number * i; multiple < upto {
-				uniqueMultiples[multiple] = nil
+				uniqueMultiples[multiple] = struct{}{}
 			}
 		}
 	}
