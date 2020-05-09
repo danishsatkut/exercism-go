ce68bc3306e6e42fad4a9dbd70e9f5110138acd9
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-10 00:43:47 +0530

    Implement SumMultiples function.

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -1,1 +1,30 @@
 package summultiples
+
+func SumMultiples(upto int, numbers ...int) int {
+	uniqueMultiples := map[int]interface{}{}
+
+	for _, number := range numbers {
+		for _, m := range multiples(number, upto) {
+			uniqueMultiples[m] = nil
+		}
+	}
+
+	total := 0
+	for m, _ := range uniqueMultiples {
+		total += m
+	}
+
+	return total
+}
+
+func multiples(n int, upto int) []int {
+	var result []int
+
+	for i := 0; i < upto; i++ {
+		if m := n * i; m < upto {
+			result = append(result, m)
+		}
+	}
+
+	return result
+}
