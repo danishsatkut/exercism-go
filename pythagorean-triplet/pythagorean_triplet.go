f326039f1a91c2fb8621b27b73d8069a1936965d
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-12 00:31:30 +0530

    Fix linting errors.

--- pythagorean-triplet/pythagorean_triplet.go
+++ pythagorean-triplet/pythagorean_triplet.go
@@ -5,6 +5,47 @@ import (
 	"sort"
 )
 
+// Range returns a list of all Pythagorean triplets with sides in the
+// range min to max inclusive.
+func Range(min, max int) []Triplet {
+	var uniqTriplets = make(map[Triplet]struct{})
+
+	for _, triplet := range primitiveTriplets(1, max) {
+		for i := min / triplet.a; i <= max/triplet.c; i++ {
+			if t := triplet.multiply(i); t.isWithinRange(min, max) && t.isValid() {
+				uniqTriplets[t] = struct{}{}
+			}
+		}
+	}
+
+	var triplets = make([]Triplet, 0, len(uniqTriplets))
+
+	for t := range uniqTriplets {
+		triplets = append(triplets, t)
+	}
+
+	return triplets
+}
+
+// Sum returns a list of all Pythagorean triplets where the sum a+b+c
+// (the perimeter) is equal to p.
+func Sum(sum int) []Triplet {
+	triplets := make([]Triplet, 0, 5)
+
+	for _, triplet := range Range(1, sum/2) {
+		if triplet.perimeter() == sum {
+			triplets = append(triplets, triplet)
+		}
+	}
+
+	sort.Slice(triplets, func(i, j int) bool {
+		return triplets[i].lessThan(triplets[j])
+	})
+
+	return triplets
+}
+
+// Triplet represents a Pythagorean triplet
 type Triplet struct {
 	a, b, c int
 }
@@ -41,11 +82,23 @@ func (t Triplet) multiply(factor int) Triplet {
 	}
 }
 
-func (t Triplet) sum() int {
+func (t Triplet) perimeter() int {
 	return t.a + t.b + t.c
 }
 
-func NewTriplet(n int) Triplet {
+func primitiveTriplets(min, max int) []Triplet {
+	var primitiveTriplets = make([]Triplet, 0, max)
+
+	for i := min; i <= max; i++ {
+		if t := newTriplet(i); t.isValid() {
+			primitiveTriplets = append(primitiveTriplets, t)
+		}
+	}
+
+	return primitiveTriplets
+}
+
+func newTriplet(n int) Triplet {
 	t := Triplet{a: n}
 
 	if n%2 == 0 {
@@ -61,52 +114,4 @@ func NewTriplet(n int) Triplet {
 	}
 
 	return t
-}
-
-func Range(min, max int) []Triplet {
-	var uniqTriplets = make(map[Triplet]struct{})
-
-	for _, triplet := range PrimitiveTriplets(1, max) {
-		for i := min / triplet.a; i <= max/triplet.c; i++ {
-			if t := triplet.multiply(i); t.isWithinRange(min, max) && t.isValid() {
-				uniqTriplets[t] = struct{}{}
-			}
-		}
-	}
-
-	var triplets = make([]Triplet, 0, len(uniqTriplets))
-
-	for t := range uniqTriplets {
-		triplets = append(triplets, t)
-	}
-
-	return triplets
-}
-
-func Sum(sum int) []Triplet {
-	triplets := make([]Triplet, 0, 5)
-
-	for _, triplet := range Range(1, sum/2) {
-		if triplet.sum() == sum {
-			triplets = append(triplets, triplet)
-		}
-	}
-
-	sort.Slice(triplets, func(i, j int) bool {
-		return triplets[i].lessThan(triplets[j])
-	})
-
-	return triplets
-}
-
-func PrimitiveTriplets(min, max int) []Triplet {
-	var primitiveTriplets = make([]Triplet, 0, max)
-
-	for i := min; i <= max; i++ {
-		if t := NewTriplet(i); t.isValid() {
-			primitiveTriplets = append(primitiveTriplets, t)
-		}
-	}
-
-	return primitiveTriplets
 }
