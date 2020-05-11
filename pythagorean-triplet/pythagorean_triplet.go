7d7365d8d2ab8bff3ec9bd82a408a5feaf7c6477
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-12 00:04:23 +0530

    Implement Sum function by refactoring Range

--- pythagorean-triplet/pythagorean_triplet.go
+++ pythagorean-triplet/pythagorean_triplet.go
@@ -1,11 +1,16 @@
 package pythagorean
 
+import (
+	"math"
+	"sort"
+)
+
 type Triplet struct {
 	a, b, c int
 }
 
 func (t Triplet) isValid() bool {
-	if t.a >= t.c || t.b >= t.c {
+	if t.a >= t.c || t.b >= t.c || t.a >= t.b {
 		return false
 	}
 
@@ -16,6 +21,18 @@ func (t Triplet) isWithinRange(min, max int) bool {
 	return t.a >= min && t.b >= min && t.c <= max
 }
 
+func (t Triplet) lessThan(other Triplet) bool {
+	if t.a != other.a {
+		return t.a < other.a
+	}
+
+	if t.b != other.b {
+		return t.b < other.b
+	}
+
+	return t.c < other.c
+}
+
 func (t Triplet) multiply(factor int) Triplet {
 	return Triplet{
 		a: t.a * factor,
@@ -24,20 +41,72 @@ func (t Triplet) multiply(factor int) Triplet {
 	}
 }
 
-var Default = Triplet{3, 4, 5}
+func (t Triplet) sum() int {
+	return t.a + t.b + t.c
+}
+
+func NewTriplet(n int) Triplet {
+	t := Triplet{a: n}
+
+	if n%2 == 0 {
+		sq := math.Pow(float64(n)/2, 2)
+
+		t.b = int(sq - 1)
+		t.c = int(sq + 1)
+	} else {
+		sq := math.Pow(float64(n), 2) / 2
+
+		t.b = int(sq - 0.5)
+		t.c = int(sq + 0.5)
+	}
+
+	return t
+}
 
 func Range(min, max int) []Triplet {
-	var triplets = make([]Triplet, 0, max)
+	var uniqTriplets = make(map[Triplet]struct{})
 
-	for i := min / Default.a; i <= max/Default.c; i++ {
-		if t := Default.multiply(i); t.isWithinRange(min, max) && t.isValid() {
-			triplets = append(triplets, t)
+	for _, triplet := range PrimitiveTriplets(1, max) {
+		for i := min / triplet.a; i <= max/triplet.c; i++ {
+			if t := triplet.multiply(i); t.isWithinRange(min, max) && t.isValid() {
+				uniqTriplets[t] = struct{}{}
+			}
 		}
+	}
+
+	var triplets = make([]Triplet, 0, len(uniqTriplets))
+
+	for t := range uniqTriplets {
+		triplets = append(triplets, t)
 	}
 
 	return triplets
 }
 
 func Sum(sum int) []Triplet {
-	return nil
+	triplets := make([]Triplet, 0, 5)
+
+	for _, triplet := range Range(1, sum/2) {
+		if triplet.sum() == sum {
+			triplets = append(triplets, triplet)
+		}
+	}
+
+	sort.Slice(triplets, func(i, j int) bool {
+		return triplets[i].lessThan(triplets[j])
+	})
+
+	return triplets
+}
+
+func PrimitiveTriplets(min, max int) []Triplet {
+	var primitiveTriplets = make([]Triplet, 0, max)
+
+	for i := min; i <= max; i++ {
+		if t := NewTriplet(i); t.isValid() {
+			primitiveTriplets = append(primitiveTriplets, t)
+		}
+	}
+
+	return primitiveTriplets
 }
