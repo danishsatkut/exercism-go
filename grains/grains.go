a0b8dbbb64d5b134d12b3d2491ee24331a27c6f6
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-14 23:06:08 +0530

    Remove unnecessary type conversions.

--- grains/grains.go
+++ grains/grains.go
@@ -12,10 +12,10 @@ func Square(n int) (uint64, error) {
 		return 0, errors.New("invalid square")
 	}
 
-	return uint64(1 << uint(n-1)), nil
+	return 1 << uint(n-1), nil
 }
 
 // Total calculates the total number of grains for the entire chessboard
 func Total() uint64 {
-	return uint64((1 << totalSquares) - 1)
+	return (1 << totalSquares) - 1
 }
