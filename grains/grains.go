28575d282fe8343c345981df1558f2dacaa115b9
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-05-15 16:20:29 +0530

    Remove const totalSquares.

--- grains/grains.go
+++ grains/grains.go
@@ -4,11 +4,9 @@ import (
 	"errors"
 )
 
-const totalSquares = 64
-
 // Square calculates the number of grains for a given square
 func Square(n int) (uint64, error) {
-	if n < 1 || n > totalSquares {
+	if n < 1 || n > 64 {
 		return 0, errors.New("invalid square")
 	}
 
@@ -17,5 +15,5 @@ func Square(n int) (uint64, error) {
 
 // Total calculates the total number of grains for the entire chessboard
 func Total() uint64 {
-	return 1<<totalSquares - 1
+	return 1<<64 - 1
 }
