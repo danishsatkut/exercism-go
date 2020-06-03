cb15a1d7ddfdabbfb6e6d27eaffcc10b09bbd82a
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-06-03 22:11:14 +0530

    Implement nth prime functionality.

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -1,1 +1,28 @@
 package prime
+
+func Nth(n int) (int, bool) {
+	if n == 0 {
+		return 0, false
+	}
+
+	primes := make([]int, 0, n)
+	primes = append(primes, 2)
+
+	for i := 3; len(primes) <= n; i++ {
+		if isPrime(primes, i) {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes[n-1], true
+}
+
+func isPrime(primes []int, number int) bool {
+	for _, prime := range primes {
+		if number % prime == 0 {
+			return false
+		}
+	}
+
+	return true
+}
