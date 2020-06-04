29d5be8cc72ff42e2b223c287db1135966012932
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-06-05 00:07:52 +0530

    Implement functionality to validate ISBN.

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -1,1 +1,39 @@
 package isbn
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func IsValidISBN(isbn string) bool {
+	isbn = strings.ReplaceAll(isbn, "-", "")
+
+	if len(isbn) != 10 {
+		return false
+	}
+
+	if unicode.IsLetter(rune(isbn[9])) && isbn[9] != 'X' {
+		return false
+	}
+
+	checksum := 0
+	for i, r := range isbn {
+		if i == 9 && r == 'X' {
+			checksum += 10
+		} else {
+			v, err := strconv.Atoi(string(r))
+			if err != nil {
+				return false
+			}
+
+			checksum += v * (10 - i)
+		}
+	}
+
+	if checksum%11 == 0 {
+		return true
+	}
+
+	return false
+}
