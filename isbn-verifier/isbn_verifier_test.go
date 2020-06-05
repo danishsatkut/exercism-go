8912623de7f8f47de118bd865fa9e17fb79bfcca
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-06-05 17:44:39 +0530

    Implement ISBN-10 to ISBN-13 converter
    - Added ConvertToISBN13 function that performs conversion to ISBN-13.
    - Added tests for ISBN-13 conversion.

--- isbn-verifier/isbn_verifier_test.go
+++ isbn-verifier/isbn_verifier_test.go
@@ -16,6 +16,27 @@ func TestIsValidISBN(t *testing.T) {
 	}
 }
 
+func TestConvertToISBN13(t *testing.T) {
+	for _, test := range testCases {
+		observed, err := ConvertToISBN13(test.isbn)
+
+		if test.expected {
+			if observed == test.isbn13 {
+				t.Logf("PASS: %s", test.description)
+			} else {
+				t.Errorf("FAIL: %s\nConvertToISBN13(%q)\nExpected: %q, Actual: %q",
+					test.description, test.isbn, test.isbn13, observed)
+			}
+		} else {
+			if err == nil {
+				t.Errorf("FAIL: Expected error for %q", test.isbn)
+			} else {
+				t.Logf("PASS: %s", test.description)
+			}
+		}
+	}
+}
+
 func BenchmarkIsValidISBN(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, n := range testCases {
