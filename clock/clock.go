763a0ab5539fe2d14ab032011f7e59cafb84f3bb
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-06-11 22:25:19 +0530

    Refactor to remove hours property for clock.

--- clock/clock.go
+++ clock/clock.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+// Clock represents a clock with hour and minutes
+type Clock struct {
+	minutes int
+}
+
 // New creates a clock with the specified hour and minutes
 func New(hour, minutes int) Clock {
 	hour = hour + (minutes / 60)
@@ -19,24 +24,19 @@ func New(hour, minutes int) Clock {
 		hour = 24 + hour
 	}
 
-	return Clock{hour, minutes}
-}
-
-// Clock represents a clock with hour and minutes
-type Clock struct {
-	hour, minutes int
+	return Clock{hour*60 + minutes}
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hour, c.minutes)
+	return fmt.Sprintf("%02d:%02d", c.minutes/60, c.minutes%60)
 }
 
 // Add adds specified minutes to the clock
 func (c Clock) Add(minutes int) Clock {
-	return New(c.hour, c.minutes+minutes)
+	return New(0, c.minutes+minutes)
 }
 
 // Subtract subtracts specified minutes from the clock
 func (c Clock) Subtract(minutes int) Clock {
-	return New(c.hour, c.minutes-minutes)
+	return New(0, c.minutes-minutes)
 }
