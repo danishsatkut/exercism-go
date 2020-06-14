dcc7b7c65e44197b09c30ee32b50771e88802413
Danish Aziz Satkut <danish_satkut@hotmail.com>
2020-06-14 12:05:09 +0530

    Implement concurrent letter frequency

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,5 +1,7 @@
 package letter
 
+import "sync"
+
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -10,5 +12,22 @@ func Frequency(s string) FreqMap {
 	for _, r := range s {
 		m[r]++
 	}
+	return m
+}
+
+func ConcurrentFrequency(texts []string) FreqMap {
+	mu := sync.RWMutex{}
+	m := FreqMap{}
+
+	for _, text := range texts {
+		go func(t string) {
+			for _, r := range t {
+				mu.Lock()
+				m[r]++
+				mu.Unlock()
+			}
+		}(text)
+	}
+
 	return m
 }
